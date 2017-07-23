28c50ae69ba83a42e777a3552dbae06738398467
fernandoans <fernando.anselmo74@gmail.com>
2017-07-23 19:08:28 -0300

    Versão Inicial

--- src/Principal.kt
+++ src/Principal.kt
@@ -1,11 +1,15 @@
+import java.awt.*
 import javax.swing.*
 import java.net.URL
-import java.awt.BorderLayout
+import javax.swing.border.Border
+
 /**
  * Projeto: Calculadora Financeira
  * Desenvolvedor: Fernando Anselmo
  * Função: Janela Inicial
- * Vídeo Disponível:
+ * Vídeos Disponíveis no meu canal
+ *   1. Criação do Visor: https://www.youtube.com/watch?v=yAWLiyPY-28&t=25s
+ *   2. Criação dos Botões: https://www.youtube.com/watch?v=nhH2sOIQnyY&t=25s
  */
 class Principal private constructor() :
         JFrame("Calculadora Finaceira") {
@@ -19,7 +23,14 @@ class Principal private constructor() :
             painel[i]?.setNumero(("" + i)[0])
             visor.add(painel[i])
         }
+        val pnBotoes = JPanel()
+        pnBotoes.layout = GridLayout(4, 10)
+        for (i in 1..4)
+            for (j in 1..10)
+                pnBotoes.add(Botao("F1:"+i+","+j, "P:"+i+","+j, "F2:"+i+","+j ))
+
         this.add(visor, BorderLayout.NORTH)
+        this.add(pnBotoes)
         this.isVisible = true
     }
     companion object {
@@ -30,14 +41,59 @@ class Principal private constructor() :
     }
 
 }
-internal  class Painel : JPanel() {
+internal class Botao constructor(fnA1: String, fnP: String, fnA2: String): JPanel() {
+    var fnAlternativa1 = JLabel(fnA1, SwingConstants.CENTER)
+    var fnPrimaria = JLabel(fnP, SwingConstants.CENTER)
+    var fnAlternativa2 = JLabel(fnA2, SwingConstants.CENTER)
+
+    init {
+        background = Color.BLACK
+        layout = GridLayout(3, 1)
+        border = BordaBotao()
+
+        fnAlternativa1.font = Font("SansSerif", Font.BOLD, 12)
+        fnAlternativa1.foreground = Color.YELLOW
+        this.add(fnAlternativa1)
+
+        fnPrimaria.font = Font("SansSerif", Font.BOLD, 20)
+        fnPrimaria.foreground = Color.WHITE
+        this.add(fnPrimaria)
+
+        fnAlternativa2.font = Font("SansSerif", Font.BOLD, 12)
+        fnAlternativa2.foreground = Color.BLUE
+        this.add(fnAlternativa2)
+    }
+}
+internal  class BordaBotao: Border {
+    override fun getBorderInsets(p0: Component?): Insets {
+        return Insets(6, 6, 6, 6)
+    }
+    override fun isBorderOpaque(): Boolean {
+        return true
+    }
+
+    override fun paintBorder(c: Component,
+         g: Graphics, x: Int, y: Int, w: Int, h: Int) {
+        g.color = Color.WHITE
+        g.drawLine(x+1, y+3, x+1, y+h-4)
+        g.drawLine(x+3, y+1, x+w-4, y+1)
+        g.drawLine(x+1, y+3, x+3, y+1)
+        g.drawLine(x+1, y+h-4, x+3, y+h-2)
+        g.color = Color.GRAY
+        g.drawLine(x+w-2, y+3, y+w-2, y+h-4)
+        g.drawLine(x+3, y+h-2, x+w-4, y+h-2)
+        g.drawLine(x+w-4, y+1, x+w-2, y+3)
+        g.drawLine(x+w-2, y+h-4, x+w-4, y+h-2)
+    }
+}
+internal class Painel : JPanel() {
     fun setNumero(valor: Char) {
         val fundo = JLabel()
         fundo.icon = Atributo.getImage("lcd_$valor.png")
         this.add(fundo)
     }
 }
-internal object  Atributo {
+internal object Atributo {
     fun getImage(s: String): ImageIcon? {
         val url = getResource("imagens/" + s)
         if (url != null)
@@ -48,6 +104,11 @@ internal object  Atributo {
         return ClassLoader.getSystemResource(s)
     }
 }
+
+
+
+
+
 
 
 
